8d180d4f0aa9282c280b65093a0c883cbe9db0c2
clinflash <88610641+clinflash@users.noreply.github.com>
2024-12-16 02:35:32 +0000

    Github Actions build and deploy

---
+++ assets/app_index.md.Brt1OEJm.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a2 as p}from"./chunks/framework.CbocFKz0.js";const f=JSON.parse('{"title":"App","description":"","frontmatter":{},"headers":[],"relativePath":"app/index.md","filePath":"app/index.md"}'),o={name:"app/index.md"},r=p("",2),n=[r];function i(_,l,s,c,d,h){return t(),a("div",null,n)}const u=e(o,[["render",i]]);export{f as __pageData,u as default};
